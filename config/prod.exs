59db4e055297ce2a7f47e31f16ee1b316ded9188
Max Grønlund <maxgronlund@bigmax.local>
2022-07-13 18:07:51 +0200

    initial commit

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ config :run_web, RunWeb.Endpoint,
   url: [scheme: "https", host: "pure-forest-93343.herokuapp.com", port: System.get_env("PORT")],
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
-# cache_static_manifest: "priv/static/cache_manifest.json"
+cache_static_manifest: "priv/static/cache_manifest.json"
 
 # ## SSL Support
 #
--- mix.exs
+++ mix.exs
@@ -7,7 +7,15 @@ defmodule Run.Umbrella.MixProject do
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      aliases: aliases()
+      aliases: aliases(),
+      releases: [
+        run: [
+          applications: [
+            run: :permanent,
+            run_web: :permanent
+          ]
+        ]
+      ]
     ]
   end
 
